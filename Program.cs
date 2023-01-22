809f6c2fecebd11068354b59962b05091f9a54da
Okoro Nnamdi <okoronnamdi4044@gmail.com>
2023-01-22 11:04:37 +0100

    First Commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+public delegate int NumberChanger(int N);
+namespace DelegateExample
+{
+    public  class Program
+    {
+        static void Main(string[] args)
+        {
+            //create delegate instances
+            NumberChanger nc1 = new NumberChanger(AddNum);
+            NumberChanger nc2 = new NumberChanger(MULNum);
+
+            //calling the methods using the delegate objects
+            nc1(25);
+            Console.WriteLine("Value of Num: {0}",GetNum());
+            nc2(5);
+            Console.WriteLine("Value of Num: {0}",GetNum());
+            Console.ReadKey();
+        }
+        static int num = 10;
+        public static int AddNum(int p)
+        {
+            return num += p;
+        }
+        public static int MULNum(int p)
+        {
+            return num *= p;
+        }
+        public static int GetNum()
+        {
+            return num;
+        }
+
+    }
+    
+
+}
